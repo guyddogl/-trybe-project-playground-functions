ad7fb4497d6098882f14f5a7194310260e2956d8
Guyddo <guyddogl@gmail.com>
2022-05-18 17:44:39 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,28 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers) {
   // seu código aqui
+  if (arrayNumbers.length === 11) {
+    for (let value of arrayNumbers) {
+      if (value >= 0 && value <= 9) {
+        let count = 0;
+        for (let countNumber of arrayNumbers) {
+          if (value === countNumber) {
+            count += 1;
+          }
+        }
+        if (count >= 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      } else {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    return '(' + arrayNumbers[0] + arrayNumbers[1] + ') ' + arrayNumbers[2] + arrayNumbers[3] + arrayNumbers[4] + arrayNumbers[5] + arrayNumbers[6] + '-' + arrayNumbers[7] + arrayNumbers[8] + arrayNumbers[9] + arrayNumbers[10];
+  }
+  return 'Array com tamanho incorreto.'
 }
+// let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// console.log("Desafio 11: ", generatePhoneNumber(arrayNumbers));
 
 // Desafio 12
 function triangleCheck() {
