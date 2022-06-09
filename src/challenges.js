ae9ab3c78d466d56ebb5e28782b2405211b860d4
Guyddo <guyddogl@gmail.com>
2022-06-09 11:52:46 -0300

    Refatorando o código

--- src/challenges.js
+++ src/challenges.js
@@ -1,82 +1,43 @@
 // Desafio 1
-function compareTrue(param1, param2) {
-  // seu código aqui
-  return param1 && param2;
-}
-// let param1 = true;
-// let param2 = true;
-// console.log("Desafio 1: " + compareTrue(param1, param2));
+const compareTrue = (param1, param2) => param1 && param2;
 
 // Desafio 2
-function calcArea(base, height) {
-  // seu código aqui
-  return ((base * height) / 2);
-}
-// let base = 10;
-// let height = 50;
-// console.log("Desafio 2: " + calcArea(base, height));
+const calcArea = (base, height) => ((base * height) / 2);
 
 // Desafio 3
-function splitSentence(string) {
-  // seu código aqui
-  return string.split(' '); // https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254
-}
-// let string = "go Trybe";
-// console.log("Desafio 3: ", splitSentence(string));
+const splitSentence = (string) => string.split(' ');
 
 // Desafio 4
-function concatName(arrayNomes) {
-  // seu código aqui
-  return `${arrayNomes[arrayNomes.length - 1]}, ${arrayNomes[0]}`;
-}
-// let arrayNomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-// console.log("Desafio 4: " + concatName(arrayNomes));
+const concatName = (arrayNomes) => `${arrayNomes[arrayNomes.length - 1]}, ${arrayNomes[0]}`;
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  // seu código aqui
-  return ((wins * 3) + ties);
-}
-// let wins = 14;
-// let ties = 8;
-// console.log("Desafio 5: ", footballPoints(wins, ties));
+const footballPoints = (wins, ties) => ((wins * 3) + ties);
 
 // Desafio 6
-function highestCount(arrayNumeros) {
-  // seu código aqui
-  let maiorValor = Math.max(...arrayNumeros); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+const highestCount = (arrayNumeros) => {
+  let maiorValor = Math.max(...arrayNumeros);
   let count = 0;
   for (let indexNumber of arrayNumeros) {
     if (indexNumber === maiorValor) count += 1;
   }
   return count;
-}
-// let arrayNumeros = [9, 1, 2, 3, 9, 5, 7];
-// console.log("Desafio 6: ", highestCount(arrayNumeros));
+};
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  // seu código aqui
-  let distanciaMouseCat1 = Math.abs(cat1 - mouse); // https://www.w3big.com/pt/jsref/jsref-abs.html
-  let distanciaMouseCat2 = Math.abs(cat2 - mouse);
+const catAndMouse = (mouse, cat1, cat2) => {
   let resultado = '';
-  if (distanciaMouseCat1 < distanciaMouseCat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     resultado = 'cat1';
-  } else if (distanciaMouseCat2 < distanciaMouseCat1) {
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
     resultado = 'cat2';
   } else {
     resultado = 'os gatos trombam e o rato foge';
   }
   return resultado;
-}
-// let mouse = 1;
-// let cat1 = 0;
-// let cat2 = 2;
-// console.log("Desafio 7: ", catAndMouse(mouse, cat1, cat2));
+};
 
 // Desafio 8
-function fizzBuzz(arrayFizzBuzz) {
-  // seu código aqui
+const fizzBuzz = (arrayFizzBuzz) => {
   let fizzBuzzResultado = [];
   for (let indexNumber of arrayFizzBuzz) {
     if ((indexNumber % 3) === 0 && (indexNumber) % 5 === 0) fizzBuzzResultado.push('fizzBuzz');
@@ -85,50 +46,41 @@ function fizzBuzz(arrayFizzBuzz) {
     else fizzBuzzResultado.push('bug!');
   }
   return fizzBuzzResultado;
-}
-// let arrayFizzBuzz = [2, 15, 7, 9, 45];
-// console.log("Desafio 8: ", fizzBuzz(arrayFizzBuzz));
+};
 
 // Desafio 9
-function encode(stringEncodeDecode) {
-  // seu código aqui
-  let aTo1 = stringEncodeDecode.replace(/a/g, 1); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+const encode = (stringEncodeDecode) => {
+  let aTo1 = stringEncodeDecode.replace(/a/g, 1);
   let eTo2 = aTo1.replace(/e/g, 2);
   let iTo3 = eTo2.replace(/i/g, 3);
   let oTo4 = iTo3.replace(/o/g, 4);
   return oTo4.replace(/u/g, 5);
-}
-function decode(stringEncodeDecode) {
-  // seu código aqui
+};
+
+const decode = (stringEncodeDecode) => {
   let oneToA = stringEncodeDecode.replace(/1/g, 'a');
   let twoToE = oneToA.replace(/2/g, 'e');
   let threeToI = twoToE.replace(/3/g, 'i');
   let fourToO = threeToI.replace(/4/g, 'o');
   return fourToO.replace(/5/g, 'u');
-}
-// let stringEncodeDecode = "hi there!";
-// console.log("Desafio 9: Encode: ", encode(stringEncodeDecode), " Decode: ", decode(stringEncodeDecode));
+};
 
 // Desafio 10
-function techList(techs, name) {
-  // seu código aqui
+const techList = (techs, name) => {
   if (techs.length !== 0) {
-    techs.sort(); // https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a
+    techs.sort();
     let arrayTechs = [];
     for (let indexTechs of techs) {
       let objTechs = {
         tech: indexTechs,
-        name, // Mentoria. Se propriedade e valor forem iguais não precisa repetir o nome (name: name)
+        name,
       };
       arrayTechs.push(objTechs);
     }
     return arrayTechs;
   }
   return 'Vazio!';
-}
-// let name = "Lucas";
-// let techs = ["React", "Jest", "HTML", "CSS", "JavaScript"];
-// console.log("Desafio 10: ", techList(techs, name));
+};
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,11 @@
 // Desafio 11
-function generatePhoneNumber(arrayNumbers) {
-  // seu código aqui
+const formatPhoneNumber = (arrayNumbers) => {
+  const numbers = arrayNumbers.join('');
+  const phoneNumber = numbers.match(/^(\d{2})(\d{5})(\d{4})$/);
+  return `(${phoneNumber[1]}) ${phoneNumber[2]}-${phoneNumber[3]}`;
+};
+
+const generatePhoneNumber = (arrayNumbers) => {
   if (arrayNumbers.length === 11) {
     for (let value of arrayNumbers) {
       if (value >= 0 && value <= 9) {
@@ -13,29 +18,23 @@ function generatePhoneNumber(arrayNumbers) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }
-    return `(${arrayNumbers[0]}${arrayNumbers[1]}) ${arrayNumbers[2]}${arrayNumbers[3]}${arrayNumbers[4]}${arrayNumbers[5]}${arrayNumbers[6]}-${arrayNumbers[7]}${arrayNumbers[8]}${arrayNumbers[9]}${arrayNumbers[10]}`;
+    return formatPhoneNumber(arrayNumbers);
   }
   return 'Array com tamanho incorreto.';
-}
-// let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-// console.log("Desafio 11: ", generatePhoneNumber(arrayNumbers));
+};
 
 // Desafio 12
-function triangleCheck(linaA, lineB, lineC) {
-  // seu código aqui
+const triangleCheck = (linaA, lineB, lineC) => {
   let sumlineAlineB = linaA + lineB;
   let sumLineAlineC = linaA + lineC;
   let sumLineBlineC = lineB + lineC;
   if (linaA > sumLineBlineC || lineB > sumLineAlineC || lineC > sumlineAlineB) return false;
   return true;
-}
-// let lineA = 10, lineB = 14, lineC = 8;
-// console.log("Desafio 12: ", triangleCheck(lineA, lineB, lineC));
+};
 
 // Desafio 13
-function hydrate(string) {
-  // seu código aqui
-  let reg = /\d+/g; // https://www.youtube.com/watch?v=pfkkdzeyx6U
+const hydrate = (string) => {
+  let reg = /\d+/g;
   let arrayNumbers = string.match(reg);
   let sumNumbers = 0;
   for (let number of arrayNumbers) {
@@ -43,9 +42,7 @@ function hydrate(string) {
     sumNumbers += numberInt;
   }
   return sumNumbers === 1 ? `${sumNumbers} copo de água` : `${sumNumbers} copos de água`;
-}
-// let string = '1 cachaça, 5 cervejas e 1 copo de vinho';
-// console.log('Desafio 13: ', hydrate(string));
+};
 
 module.exports = {
   generatePhoneNumber,
