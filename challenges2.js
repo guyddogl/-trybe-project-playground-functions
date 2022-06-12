d183eaf8227c328964c2dc08a812ffb7742c2d71
guyddogl <guyddogl@gmail.com>
2022-06-12 15:35:07 -0300

    Refatorando a função generatePhoneNumber

--- challenges2.js
+++ challenges2.js
@@ -1,26 +1,44 @@
-// Desafio 11
 const formatPhoneNumber = (arrayNumbers) => {
   const numbers = arrayNumbers.join('');
   const phoneNumber = numbers.match(/^(\d{2})(\d{5})(\d{4})$/);
   return `(${phoneNumber[1]}) ${phoneNumber[2]}-${phoneNumber[3]}`;
 };
 
-const generatePhoneNumber = (arrayNumbers) => {
-  if (arrayNumbers.length === 11) {
-    for (let value of arrayNumbers) {
-      if (value >= 0 && value <= 9) {
-        let count = 0;
-        for (let countNumber of arrayNumbers) {
-          if (value === countNumber) count += 1;
-        }
-        if (count >= 3) return 'não é possível gerar um número de telefone com esses valores';
-      } else {
-        return 'não é possível gerar um número de telefone com esses valores';
-      }
-    }
-    return formatPhoneNumber(arrayNumbers);
+const arrayCountNumbers = (value, arrayNumbers) => {
+  let count = 0;
+  for (let countNumber of arrayNumbers) {
+    if (value === countNumber) count += 1;
   }
-  return 'Array com tamanho incorreto.';
+  if (count >= 3) throw new Error('não é possível gerar um número de telefone com esses valores');
+};
+
+const arrayRepeatNumberValidate = (arrayNumbers) => {
+  for (let value of arrayNumbers) {
+    arrayCountNumbers(value, arrayNumbers);
+  }
+};
+
+const arrayMinMaxValidate = (arrayNumbers) => {
+  for (let value of arrayNumbers) {
+    if (value < 0 || value > 9) {
+      throw new Error('não é possível gerar um número de telefone com esses valores');
+    }
+  }
+};
+
+const arrayLengthValidate = (arrayNumbers) => {
+  if (arrayNumbers.length !== 11) throw new Error('Array com tamanho incorreto.');
+};
+
+const generatePhoneNumber = (arrayNumbers) => {
+  try {
+    arrayLengthValidate(arrayNumbers);
+    arrayMinMaxValidate(arrayNumbers);
+    arrayRepeatNumberValidate(arrayNumbers);
+    return formatPhoneNumber(arrayNumbers);
+  } catch (error) {
+    return error.message;
+  }
 };
 
 // Desafio 12
@@ -42,10 +60,4 @@ const hydrate = (string) => {
     sumNumbers += numberInt;
   }
   return sumNumbers === 1 ? `${sumNumbers} copo de água` : `${sumNumbers} copos de água`;
-};
-
-module.exports = {
-  generatePhoneNumber,
-  hydrate,
-  triangleCheck,
 };
