a494e88ab79d5fe77ec32cd09e807501aea20aee
Guyddo <guyddogl@gmail.com>
2022-06-13 15:05:05 -0300

    Cria primeiro teste para a função compareTrue do Desafio 1

--- challenges.test.js
+++ challenges.test.js
@@ -1,1 +1,9 @@
 const challenges = require('./challenges');
+const { compareTrue } = require('./challenges');
+
+describe('Desafio 1: Crie uma função usando o operador &&', () => {
+  test('Retorna false quando executar a função compareTrue com um parâmetro false e outro true', () => {
+    expect(false).toBe(compareTrue(false, true));
+    expect(false).toBe(compareTrue(true, false));
+  });
+});
