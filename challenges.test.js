740ef02385526d7214773871f2060d523737f8bf
Guyddo <guyddogl@gmail.com>
2022-06-13 16:43:56 -0300

    Conclusão dos testes para função compareTrue

--- challenges.test.js
+++ challenges.test.js
@@ -1,9 +1,15 @@
-const challenges = require('./challenges');
-const { compareTrue } = require('./challenges');
+const { compareTrue, calcArea, splitSentence } = require('./challenges');
 
+// Testes compareTrue
 describe('Desafio 1: Crie uma função usando o operador &&', () => {
   test('Retorna false quando executar a função compareTrue com um parâmetro false e outro true', () => {
     expect(false).toBe(compareTrue(false, true));
     expect(false).toBe(compareTrue(true, false));
+  });
+  test('Retorna false quando executar a função compareTrue com os parâmetros false e false', () => {
+    expect(false).toBe(compareTrue(false, false));
+  });
+  test('Retorna true quando executar a função compareTrue com os parâmetros true e true', () => {
+    expect(true).toBe(compareTrue(true, true));
   });
 });
