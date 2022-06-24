0cbd4cb2a0ad9b126fb7f7dd0fc39f1e6fed304d
Guyddo <guyddogl@gmail.com>
2022-06-24 17:56:19 -0300

    Concluí testes fizzBuzz e numbersModule

--- challenges.test.js
+++ challenges.test.js
@@ -97,4 +97,10 @@ describe('Desafio 8: Crie uma função que recebe um array de números e retorna
   test('Retorna as strings [\'bug!\', \'fizzBuzz\', \'bug!\', \'fizz\', \'fizzBuzz\'] quando é passado os parâmetros [2, 15, 7, 9, 45] para função fizzBuzz', () => {
     expect(fizzBuzz([2, 15, 7, 9, 45])).toEqual(['bug!', 'fizzBuzz', 'bug!', 'fizz', 'fizzBuzz']);
   });
+  test('Retorna as strings [\'bug!\', \'fizz\'] quando é passado os parâmetros [7, 9] para função fizzBuzz', () => {
+    expect(fizzBuzz([7, 9])).toEqual(['bug!', 'fizz']);
+  });
+  test('Retorna as strings [\'fizz\', \'buzz\'] quando é passado os parâmetros [9, 25] para função fizzBuzz', () => {
+    expect(fizzBuzz([9, 25])).toEqual(['fizz', 'buzz']);
+  });
 });
